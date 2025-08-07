72f4dbf3ce396b4ca33e7b1ea122c4b4641d9409
JD @ RatherChat <jdgresse@rather.chat>
2025-08-07 12:13:32 +0200

    Remove nonexistent data app from root URLs

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("apps.data.urls", namespace="data")),
     path("auth/", include("apps.custom_user.urls", namespace="custom_user")),
     # Example for your apps:
     # path('dashboard/', include('apps.dashboard.urls')),
